8b796dba553b510cfd95f58c81bd261f98a171ec
thaxz <thaxzt@gmail.com>
2023-08-02 19:41:49 -0300

    first commit

---
+++ SpaceShooter/SpaceShooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceShooterApp.swift
+//  SpaceShooter
+//
+//  Created by thaxz on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceShooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
